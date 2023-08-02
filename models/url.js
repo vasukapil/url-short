6a5b1675705c9c5434f2a8ec237d69ffc36a7a76
vasukapil2196 <vasukapil2196@gmail.com>
2023-08-02 18:45:02 +0530

    url shortner done

---
+++ models/url.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const urlSchema = new mongoose.Schema({
+  shortId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  redirectURL: {
+    type: String,
+    required: true,
+  },
+
+  visitHistory: [
+    {
+      timestamp: {
+        type: Number,
+
+      },
+    },
+  ],
+},
+{timestamp: true}
+);
+
+
+const url = mongoose.model('url',urlSchema);
+module.exports = url
